fc834f38210ee8579bfc43ef7af55c40ece3ccc7
abdallah <abdallahtolba17@gmail.com>
2025-03-27 00:47:42 +0200

    complete data source

--- lib/core/network/api_constances.dart
+++ lib/core/network/api_constances.dart
@@ -1,4 +1,6 @@
 class ApiConstances {
   static const baseUrl = 'https://ecommerce-api.edgesgate.com';
   static const homeSliderPath = '$baseUrl/homeSliders';
+  static const categoriesPath = '$baseUrl/categories?show_sub=1';
+  static const productsTopRatedPath = '$baseUrl/products/topRated';
 }
--- lib/products/data/data_source/remote_data_source/products_data_source.dart
+++ lib/products/data/data_source/remote_data_source/products_data_source.dart
@@ -2,10 +2,14 @@ import 'package:dio/dio.dart';
 import 'package:ecommerce_app/core/error/exception.dart';
 import 'package:ecommerce_app/core/network/api_constances.dart';
 import 'package:ecommerce_app/core/network/error_message_model.dart';
+import 'package:ecommerce_app/products/data/models/categories_model.dart';
 import 'package:ecommerce_app/products/data/models/home_sliders_model.dart';
+import 'package:ecommerce_app/products/data/models/products_top_rated_model.dart';
 
 abstract class BaseProductsDataSource {
   Future<List<HomeSlidersModel>> getHomeSliders();
+  Future<List<CategoriesModel>> getCategories();
+  Future<List<ProductsTopRatedModel>> getProductsTopRated();
 }
 
 class ProductsDataSource extends BaseProductsDataSource {
@@ -16,6 +20,36 @@ class ProductsDataSource extends BaseProductsDataSource {
       return List<HomeSlidersModel>.from(
         (response.data["data"] as List).map(
           (e) => HomeSlidersModel.fromJson(e),
+        ),
+      );
+    } else {
+      return throw ServerException(
+        errorMessageModel: ErrorMessageModel.fromJson(response.data),
+      );
+    }
+  }
+
+  @override
+  Future<List<CategoriesModel>> getCategories() async {
+    final response = await Dio().get(ApiConstances.categoriesPath);
+    if (response.statusCode == 200) {
+      return List<CategoriesModel>.from(
+        (response.data["data"] as List).map((e) => CategoriesModel.fromJson(e)),
+      );
+    } else {
+      return throw ServerException(
+        errorMessageModel: ErrorMessageModel.fromJson(response.data),
+      );
+    }
+  }
+
+  @override
+  Future<List<ProductsTopRatedModel>> getProductsTopRated() async {
+    final response = await Dio().get(ApiConstances.productsTopRatedPath);
+    if (response.statusCode == 200) {
+      return List<ProductsTopRatedModel>.from(
+        (response.data["data"] as List).map(
+          (e) => ProductsTopRatedModel.fromJson(e),
         ),
       );
     } else {
