102438349284a95107e4444e0d74e9a68b4777d0
abdallah <abdallahtolba17@gmail.com>
2025-04-01 18:10:55 +0200

    add theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/core/global/theme/cubit/theme_cubit.dart';
 import 'package:ecommerce_app/generated/l10n.dart';
 import 'package:ecommerce_app/l10n/cubit/local_cubit.dart';
 import 'package:flutter/material.dart';
@@ -11,24 +12,26 @@ class App extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => LocaleCubit()..loadSavedLocale()),
-        // BlocProvider(create: (context) => ThemeCubit()..loadSavedTheme()),
+        BlocProvider(create: (context) => ThemeCubit()..loadSavedTheme()),
       ],
       child: BlocBuilder<LocaleCubit, String>(
         builder: (context, locale) {
-          // return BlocBuilder<ThemeCubit, ThemeState>(
-          //   builder: (context, state) {
-          return MaterialApp(
-            title: "Notes",
-            debugShowCheckedModeBanner: false,
-            supportedLocales: S.delegate.supportedLocales,
-            localizationsDelegates: LocaleCubit.localizationsDelegates,
-            locale: Locale(locale),
-            // theme: context.read<ThemeCubit>().currentTheme(),
-            themeAnimationStyle: AnimationStyle(
-              duration: Duration(seconds: 2),
-              curve: Curves.decelerate,
-            ),
-            home: MyWidget(),
+          return BlocBuilder<ThemeCubit, ThemeState>(
+            builder: (context, state) {
+              return MaterialApp(
+                title: "Notes",
+                debugShowCheckedModeBanner: false,
+                supportedLocales: S.delegate.supportedLocales,
+                localizationsDelegates: LocaleCubit.localizationsDelegates,
+                locale: Locale(locale),
+                theme: context.read<ThemeCubit>().currentTheme(),
+                themeAnimationStyle: AnimationStyle(
+                  duration: Duration(seconds: 2),
+                  curve: Curves.decelerate,
+                ),
+                home: MyWidget(),
+              );
+            },
           );
         },
       ),
